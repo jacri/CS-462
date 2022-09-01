a66b0d3da369deacf28ef7e45d3c1b2e4483a5f6
jacri <jacri1@protonmail.com>
2022-09-01 15:20:21 -0600

    Initial commit

---
+++ Assets/Scripts/Player/RotateCamera.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    // ===== Public Variables =====================================================================
+
+    [Header("Sensitivity")]
+
+    public float hSen;
+    public float vSen;
+
+    [Space(10)]
+    [Header("System")]
+
+    public bool canLook = false;
+
+    // ===== Private Variables =====================================================================
+
+    private Transform player;
+    private Vector3 playerDelta;
+
+    private Transform cameraBoom;
+    private Vector3 cameraBoomDelta;
+
+    // ===== Start ================================================================================
+    
+    private void Start ()
+    {
+        player = transform;
+        playerDelta = Vector3.zero;
+
+        cameraBoom = player.GetChild(0);
+        cameraBoomDelta = Vector3.zero;
+    }
+
+    // ===== Update ===============================================================================
+    
+    private void Update ()
+    {
+        if (canLook)
+        {
+            playerDelta.y = Input.GetAxisRaw("Horizontal") * vSen;
+            player.Rotate(playerDelta);
+
+            cameraBoomDelta.x = Input.GetAxisRaw("Vertical") * hSen;
+            cameraBoom.Rotate(cameraBoomDelta);
+        }
+    }
+}
---
+++ Assets/Scripts/System/MenuManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // ===== Private Variables =====================================================================
+
+    //private bool paused;
+    private RotateCamera[] rots;
+
+#if UNITY_EDITOR
+    private bool paused = true;
+#else
+    private bool paused = false;
+#endif
+
+    // ===== Start ================================================================================
+    
+    private void Start ()
+    {
+        rots = FindObjectsOfType<RotateCamera>();
+    }
+
+    // ===== Update ===============================================================================
+    
+    private void Update ()
+    {
+        if (Input.GetButtonDown("Cancel"))
+            TogglePause();
+
+        if (paused && Input.GetMouseButtonDown(0))
+            Unpause();
+    }
+
+    // ===== Cursor ===============================================================================
+    
+    public void LockCursor ()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        foreach (RotateCamera r in rots)
+            r.canLook = true;
+    }
+
+    public void UnlockCursor () 
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+
+        foreach (RotateCamera r in rots)
+            r.canLook = false;
+    }
+
+    // ===== Pause ================================================================================
+
+    public void Pause () 
+    {
+        paused = true;
+        UnlockCursor();
+        Time.timeScale = 0f;
+    }
+
+    public void Unpause ()
+    {
+        paused = false;
+        LockCursor();
+        Time.timeScale = 1f;
+    }
+
+    private void TogglePause () 
+    {
+        if (paused)
+            Unpause();
+
+        else
+            Pause();
+    }
+}
