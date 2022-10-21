d178038a687abc4f4a837742eef6bb4cc338e22f
Jackson Acri <jacri1@protonmail.com>
2022-10-21 13:32:08 -0600

    Create PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // ===== Public Variables =====================================================================
 
+    public float walkSpeed;
+
+    // ===== Private Variables =====================================================================
+
+    private Transform t;
+    private Transform cameraTransform;
+
+    // ===== Start ================================================================================
+    
+    private void Start ()
+    {
+        t = transform;
+        cameraTransform = Camera.main.transform;
+    }
+
+    // ===== Update ===============================================================================
+    
+    private void Update ()
+    {
+        Vector3 v = new Vector3(Input.GetAxisRaw("Vertical"), 0f, Input.GetAxisRaw("Horizontal"));
+        v = cameraTransform.TransformDirection(v);
+        t.position += v * walkSpeed * Time.deltaTime;
+    }
 }
