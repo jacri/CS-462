e3bdfc69180717848558e38bc9b4e018edc5edd0
Jackson Acri <jacri1@protonmail.com>
2022-10-25 15:24:30 -0600

    Add jumping ability

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
     // ===== Public Variables =====================================================================
 
     public float walkSpeed;
+    public float jumpForce;
 
     // ===== Private Variables =====================================================================
 
     private Transform t;
+    private Rigidbody rb;
     private Animator anim;
     private Transform cameraTransform;
 
@@ -17,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start ()
     {
         t = transform;
+        rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         cameraTransform = Camera.main.transform;
     }
@@ -30,5 +34,26 @@ public class PlayerMovement : MonoBehaviour
         t.position += v * walkSpeed * Time.deltaTime;
 
         anim.SetFloat("Velocity", v.magnitude);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(Jump());
+    }
+
+    // ===== Jump ==================================================================================
+
+    private IEnumerator Jump () 
+    {
+        rb.AddForce(Vector3.up * jumpForce);
+
+        while (rb.velocity.y >= 0f)
+            yield return new WaitForEndOfFrame();
+
+        rb.useGravity = false;
+        rb.velocity += new Vector3(0f, -rb.velocity.y, 0f);
+
+        while (Input.GetKey(KeyCode.Space))
+            yield return new WaitForEndOfFrame();
+
+        rb.useGravity = true;
     }
 }
