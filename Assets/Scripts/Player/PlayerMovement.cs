c84e95fe852bca4a416e1b01b71e4b1ef2a36ca0
Jackson Acri <jacri1@protonmail.com>
2022-10-25 15:06:53 -0600

    Add player walk animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     // ===== Private Variables =====================================================================
 
     private Transform t;
+    private Animator anim;
     private Transform cameraTransform;
 
     // ===== Start ================================================================================
@@ -16,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start ()
     {
         t = transform;
+        anim = GetComponent<Animator>();
         cameraTransform = Camera.main.transform;
     }
 
@@ -26,5 +28,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 v = new Vector3(Input.GetAxisRaw("Vertical"), 0f, Input.GetAxisRaw("Horizontal"));
         v = cameraTransform.TransformDirection(v);
         t.position += v * walkSpeed * Time.deltaTime;
+
+        anim.SetFloat("Velocity", v.magnitude);
     }
 }
